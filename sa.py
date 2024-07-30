b7975d29602b5693769a294101715d0d53639b72
Nagesh96 <103498281+Nagesh96@users.noreply.github.com>
2024-07-30 18:19:09 +0530

    Update sa.py

--- sa.py
+++ sa.py
@@ -1,16 +1,16 @@
-
-from sqlalchemy import create engine
+from sqlalchemy import create_engine
 def test_sql_auth_connection():
-try:
-connection string = ("mssql+pyodbc://"
-"ccwuser: resum3@"
-"sdcsqld07/ccwdb?"
-"driver=ODBC+Driver+17+for+SQL+Server")
-engine create engine (connection_string)
-connection = engine.connect()
-print("SQL server authentication connection successful")
-connection.close()
-except Exception as e:
-print(f"Authentication failed: {e}")
-if_name == main":
-test sql auth_connection()
+    try:
+        connection_string = ("mssql+pyodbc://"
+        "ccwuser: resum3@"
+        "sdcsqld07/ccwdb?"
+        "driver=ODBC+Driver+17+for+SQL+Server")
+        engine = create_engine (connection_string)
+        connection = engine.connect()
+        print("SQL server authentication connection successful")
+        connection.close()
+    except Exception as e:
+        print(f"Authentication failed: {e}")
+
+if__name__ == "__main__":
+    test_sql_auth_connection()
